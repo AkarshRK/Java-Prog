3b51a44b8ae6ada6a6a7ee83fffcff0d075548b3
Akarsh <akarshrain@gmail.com>
2018-11-24 05:22:37 +0000

    parameter added in Fact

--- Fact.java
+++ Fact.java
@@ -2,8 +2,9 @@
 class Fact 
 { 
     // Method to find factorial of given number 
-    static int factorial(int n) 
+    static int factorial(int n, int c) 
     { 
+        System.out.println("Parameter added: " + c);
         int res = 1, i; 
         for (i=2; i<=n; i++) 
             res *= i; 
@@ -14,6 +15,6 @@ class Fact
     public static void main(String[] args)  
     { 
         int num = 5; 
-        System.out.println("Factorial of "+ num + " is " + factorial(5)); 
+        System.out.println("Factorial of "+ num + " is " + factorial(5, 99)); 
     } 
 } 
