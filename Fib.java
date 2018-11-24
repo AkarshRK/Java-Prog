665958605bb6e67ade44957f643092d52a8cf8b5
Akarsh <akarshrain@gmail.com>
2018-11-24 05:28:35 +0000

    added parameters in Fib method

--- Fib.java
+++ Fib.java
@@ -1,16 +1,17 @@
 //Fibonacci Series using Recursion 
 class Fib 
 { 
-    static int fib(int n) 
+    static int fib(int n, int a, int b) 
     { 
+        System.out.println("Parameter added: " + a + b);
     if (n <= 1) 
        return n; 
-    return fib(n-1) + fib(n-2); 
+    return fib(n-1, a, b) + fib(n-2, a, b); 
     } 
        
     public static void main (String args[]) 
     { 
     int n = 9; 
-    System.out.println(fib(n)); 
+    System.out.println(fib(n, 10, 20)); 
     } 
 } 
